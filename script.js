368099555e0ef2d136b17f0bc5af161e1b6f2799
yasha1w <naoapenas1@gmail.com>
2024-12-12 15:35:46 -0500

    subir arquivo

---
+++ script.js
@@ -0,0 +1,9 @@
+let num1= prompt ("digite o primeiro valor")
+let num2 = prompt ("digite o segundo numero")
+let result = num1 * num2
+
+if(num1 * num2){
+    alert("a multiplicação dos dois numeros é "+ result)
+} else {
+    alert("valor invalido")
+}
